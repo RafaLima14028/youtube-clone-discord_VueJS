a945233b2026bf2ea49ea18a3d3726fab5e9cf9d
Rafael <rafael.alv334@gmail.com>
2021-04-29 12:05:07 -0300

    Layout css

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,6 +1,18 @@
 <template>
+  <div id="grid"></div>
 </template>
 
 <script></script>
 
-<style lang="scss"></style>
+<style scoped>
+#grid {
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 71px 240px auto 240px;
+  grid-template-rows: 46px auto 52px;
+  grid-template-areas:
+    "SL SN CI CI"
+    "SL CL CD UL"
+    "SL UI CD UL";
+}
+</style>
