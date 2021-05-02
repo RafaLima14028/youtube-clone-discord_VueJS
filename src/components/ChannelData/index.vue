40d69b2b9ba5970e03f2ab71653b722384550a49
Rafael <rafael.alv334@gmail.com>
2021-05-02 19:26:56 -0300

    ChannelData Component

---
+++ src/components/ChannelData/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="channel-data">
+    <div class="messages">
+      <ChannelMessage author="Rafael Lima" date="02/05/2021" isBot hasMention>
+        Esta é uma mensagem!
+      </ChannelMessage>
+      <ChannelMessage author="Rafael Lima" date="02/05/2021" isBot>
+        Esta é uma mensagem!
+      </ChannelMessage>
+      <ChannelMessage
+        v-for="messages in 20"
+        :key="messages.id"
+        author="Rafael Lima"
+        date="02/05/2021"
+      >
+        Esta é uma mensagem!
+      </ChannelMessage>
+      <ChannelMessage author="Rafael Lima" date="02/05/2021" isBot>
+        Esta é uma mensagem!
+      </ChannelMessage>
+      <ChannelMessage author="Rafael Lima" date="02/05/2021" isBot hasMention>
+        Esta é uma mensagem!
+      </ChannelMessage>
+      <ChannelMessage author="Rafael Lima" date="02/05/2021" hasMention>
+        <Mention>Daniela Alves</Mention>, olha isso!
+      </ChannelMessage>
+    </div>
+    <div class="input-wrapper">
+      <input type="text" placeholder="Conversar em #chat-livre" />
+      <div class="icon">
+        <AtIcon :size="24" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import AtIcon from "vue-material-design-icons/At";
+import ChannelMessage from "./ChannelMessage";
+
+export default {
+  components: {
+    AtIcon,
+    ChannelMessage
+  }
+};
+
+Vue.component("Mention", {
+  template: '<span class="mention"><slot /><span>'
+})
+</script>
+
+<style lang="scss" scoped>
+.channel-data {
+  grid-area: CD;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: var(--primary);
+  flex: 1;
+}
+
+.messages {
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 46px - 68px);
+  max-height: calc(100vh - 46px - 68px);
+  overflow-y: scroll;
+  scrollbar-width: thin !important;
+
+  .channel-message:first-child {
+    margin-top: 0;
+  }
+}
+
+.input-wrapper {
+  width: 100%;
+  padding: 0 16px;
+  height: 68px;
+
+  input {
+    width: 100%;
+    height: 47px;
+    padding: 0 10px 0 57px;
+    border-radius: 5px;
+    color: var(--white);
+    background-color: var(--chat-input);
+    position: relative;
+
+    &::placeholder {
+      color: var(--grey);
+    }
+  }
+
+  .icon {
+    color: var(--grey);
+    position: relative;
+    top: -50%;
+    left: 14px;
+    transition: 0.2s;
+    width: 24px;
+  }
+}
+</style>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -5,6 +5,7 @@
     <ChannelList />
     <UserInfo />
     <ChannelInfo />
+    <ChannelData />
   </div>
 </template>
 
@@ -14,6 +15,7 @@ import ServerName from "./ServerName/index";
 import ChannelList from "./ChannelList/index";
 import UserInfo from "./UserInfo/index";
 import ChannelInfo from "./ChannelInfo/index";
+import ChannelData from "./ChannelData/index";
 
 export default {
   components: {
@@ -21,13 +23,15 @@ export default {
     ServerName,
     ChannelList,
     UserInfo,
-    ChannelInfo
+    ChannelInfo,
+    ChannelData
   }
 };
 </script>
 
 <style scoped>
 #grid {
+  overflow: hidden;
   display: grid;
   grid-template-columns: 71px 240px auto 240px;
   grid-template-rows: 46px auto 52px;
